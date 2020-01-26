c6eaad2fc1c74025a6b95294c87599fbf89bd665
rongbang <rongbang.ye@gmail.com>
2020-01-25 23:41:50 -0800

    Updates

---
+++ precache-manifest.791db902c1b6453c19a035716ac4fdf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af86cd28c6d4b016f43aac70c405112a",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "54fdba64dae52baa144d",
+    "url": "/ravenous/static/css/main.c653d343.chunk.css"
+  },
+  {
+    "revision": "5ba65b0643782bd6f9e0",
+    "url": "/ravenous/static/js/2.f0ffdcd7.chunk.js"
+  },
+  {
+    "revision": "54fdba64dae52baa144d",
+    "url": "/ravenous/static/js/main.9b699911.chunk.js"
+  },
+  {
+    "revision": "f800d3952494e032da75",
+    "url": "/ravenous/static/js/runtime-main.eb998c89.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
